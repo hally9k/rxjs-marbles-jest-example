7733be8c22a08d16ee8a3235f3d816bc560db83f
hally9k <hally9k@gmail.com>
2018-02-25 15:16:25 +1300

    Added ticker to example.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const delayer = require('./delayer')
 const { Subject } = require('rxjs/Subject')
+const { Observable } = require('rxjs/Observable')
+require('rxjs/add/observable/interval')
+require('rxjs/add/operator/take')
+require('rxjs/add/operator/delay')
 
+// Producer
 const subject = new Subject()
 
+// Observable
 const observable = delayer(subject)
 
 observable.subscribe(null, null, () => console.log('complete'))
@@ -11,3 +17,8 @@ subject.next({ value: 'alpha', delay: 1000 })
 subject.next({ value: 'beta', delay: 2000 })
 subject.next({ value: 'charlie', delay: 3000 })
 subject.next({ value: 'delta', delay: 4000 })
+
+// Ticker
+Observable.interval(1000)
+	.take(5)
+	.subscribe(() => console.log('.'))
